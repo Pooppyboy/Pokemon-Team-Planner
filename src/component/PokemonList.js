e329f57df041641bca8a2d7bd7313cc0cf10bc09
yjrandom <random.ran.dom@hotmail.com>
2021-05-29 16:53:34 +0800

    Work without rerender

--- src/component/PokemonList.js
+++ src/component/PokemonList.js
@@ -6,17 +6,17 @@ import {addToParty} from "../lib/helpers";
 function PokemonList({pokemonList, setPokemonList, party, setParty}) {
 
     useEffect(() => {
-        let temp = [...pokemonList]
+        // let temp = [...pokemonList]
         axios.get('https://pokeapi.co/api/v2/pokemon/?limit=151')
             .then(response => {
                 response.data.results.forEach(pokemon => {
                     axios.get(pokemon.url)
                         .then(url => {
-                            temp[url.data.id - 1] = url.data
+                            // temp[url.data.id - 1] = url.data
                             // setPokemonList(prevUrl => [...prevUrl, url.data])
                         })
                 })
-                setPokemonList(temp)
+                // setPokemonList(temp)
             }).catch(error => console.error(error))
     }, [])
 
